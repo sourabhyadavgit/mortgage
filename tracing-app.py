fe99bf46b181980d89baa35667260ea14e72371f
Sourabh Yadav <saurav_sori@yahoo.com>
2023-01-20 15:08:34 +0000

    uppdates with mongo and tracing

---
+++ tracing-app.py
@@ -0,0 +1,24 @@
+import json
+import kafka
+import time
+import pymongo
+from kafka import KafkaConsumer
+from kafka import KafkaProducer
+
+Tracing_TOPIC = "trace_details"
+consumer = KafkaConsumer(
+    Tracing_TOPIC,
+    bootstrap_servers="192.168.1.112:9092"
+)
+print( "--Tracing is Listening for tracing topic now--")
+while True:
+
+    for message in consumer:
+        print("Trace notification")
+        consumed_msg = json.loads(message.value.decode("utf-8"))
+        cust_id =  consumed_msg["customer_id"]
+        state = consumed_msg["state"]
+        
+        print("--------------------------------------------------------------------------------------------------------------------")
+        print(f"INFO: Tracelevel-4  at Customer_id {cust_id}  is  {state}..!! ")
+#print( " testing2")
